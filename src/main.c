765d790a52c6fba1af4b1eed675ba4b2740e22d4
Pablo Gonzalez <pablo.gonzalez@lmcontrol.fr>
2025-10-21 17:09:07 +0200

    wip: override partitions map

--- src/main.c
+++ src/main.c
@@ -3,9 +3,39 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/fs/littlefs.h>
+#include <zephyr/storage/flash_map.h>
 #include <lvgl.h>
 
 LOG_MODULE_REGISTER(main);
+void print_partition_info(void)
+{
+    const struct flash_area *fa;
+    int rc;
+
+    /* Get slot0 partition info */
+    rc = flash_area_open(FIXED_PARTITION_ID(slot0_partition), &fa);
+    if (rc == 0) {
+        printk("Slot0: offset=0x%x, size=0x%x\n", fa->fa_off, fa->fa_size);
+        flash_area_close(fa);
+    }
+
+    /* Get slot1 partition info */
+    rc = flash_area_open(FIXED_PARTITION_ID(slot1_partition), &fa);
+    if (rc == 0) {
+        printk("Slot1: offset=0x%x, size=0x%x\n", fa->fa_off, fa->fa_size);
+        flash_area_close(fa);
+    }
+
+    /* Get storage partition info */
+    rc = flash_area_open(FIXED_PARTITION_ID(storage_partition), &fa);
+    if (rc == 0) {
+        printk("Storage: offset=0x%x, size=0x%x\n", fa->fa_off, fa->fa_size);
+        flash_area_close(fa);
+    }
+}
+
 
 int main(void) {
 
