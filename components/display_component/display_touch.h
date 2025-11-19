0993e9300bced06451b75e266358789597081649
Pablo Gonzalez <pablo.gonzalez@lmcontrol.fr>
2025-11-19 11:20:34 +0100

    BREAKING CHANGE: reorganize components

---
+++ components/display_component/display_touch.h
@@ -0,0 +1,10 @@
+//
+// Created by pgonzalez on 10/20/25.
+//
+
+#ifndef CYD_ZEPHYR_TESTS_DISPLAY_TOUCH_H
+#define CYD_ZEPHYR_TESTS_DISPLAY_TOUCH_H
+
+int draw_content_touch();
+
+#endif //CYD_ZEPHYR_TESTS_DISPLAY_TOUCH_H
